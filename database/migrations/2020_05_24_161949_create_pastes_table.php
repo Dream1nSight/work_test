719cea06a6f18ef9e7203ef604c5867bc2dc240b
Dream1nSight <omg.demontut@gmail.com>
2020-05-24 23:24:03 +0700

    added auth and Paste model

---
+++ database/migrations/2020_05_24_161949_create_pastes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePastesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pastes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->timestamp('expiries_at');
+            $table->boolean('is_public');
+            $table->string('link');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pastes');
+    }
+}
